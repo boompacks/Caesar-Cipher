a8af92662b19747792bf56b9b29e9873da6e91b0
gabriele agosta <g.agosta02@gmail.com>
2022-09-18 18:44:51 +0200

    Added functionality to cipher a text through Caeser Cipher

---
+++ caeser-cipher.rb
@@ -0,0 +1,51 @@
+def shift_alphabet(alphabet, shift)
+  shifted_alphabet = alphabet.dup
+  shift.times do 
+    char = shifted_alphabet.shift
+    shifted_alphabet.push(char)
+  end 
+  alphabet = Hash[alphabet.zip(shifted_alphabet)]
+end
+
+
+def caeser_cipher(string, shift)
+  ciphered_string = []
+
+  lower_alphabet = ('a'..'z').to_a
+  lower_alphabet = shift_alphabet(lower_alphabet, shift)
+
+  upper_alphabet = ('A'..'Z').to_a
+  upper_alphabet = shift_alphabet(upper_alphabet, shift)
+
+  string.chars.each do |char|
+    if /[[:upper:]]/.match(char)
+      ciphered_string.push(upper_alphabet[char])
+    elsif char == " "
+      ciphered_string.push(" ")
+    elsif /[[:lower:]]/.match(char)
+      ciphered_string.push(lower_alphabet[char])
+    else
+      ciphered_string.push(char)
+    end
+  end
+  return ciphered_string.join('')
+end
+
+
+def main()
+  puts "Insert the string: "
+  string = gets.chomp
+  puts "Insert the shift: "
+  shift = gets.chomp.to_i
+  ciphered_string = caeser_cipher(string, shift)
+  puts ciphered_string
+end
+
+
+main()
+
+
+=begin 
+// inserire all'interno di un hash le lettere dell'alfabeto, e il corrispondente valore
+// per prendere le lettere dell'alfabeto e shiftarle, posso fare uno slice che toglie la roba dall'inizio dello shift, e lo riposiziona alla fine
+=end
